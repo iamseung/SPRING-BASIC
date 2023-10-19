9d6cf3e1c23966f4963dcdd9de143ea6c4237560
iamseung <seesungseok@naver.com>
2023-10-19 23:57:09 +0900

    싱글톤 패턴 테스트 추가

--- src/test/java/com/core/singleton/SingletonService.java
+++ src/test/java/com/core/singleton/SingletonService.java
@@ -4,11 +4,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class SingletonService {
-    // 싱글톤은 자기 자신을 내부의 프라이빗으로 가지고 있음
+    // 싱글톤은 자기 자신을 내부의 private 으로 가지고 있음
     private static final SingletonService instance = new SingletonService();
 
    public static SingletonService getInstance() {
        return instance;
    }
 
+   private SingletonService() {
+   }
+
+   public void logic() {
+       System.out.println("싱글톤 객체 로직 호출");
+   }
 }
